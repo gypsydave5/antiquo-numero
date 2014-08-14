9dc912f68af70103dba9fe96b1bb780a69517c57
David Wickes <dave.wickes@gmail.com>
2014-08-14 22:42:35 +0100

    can now count to 20; test coverage extended for multiples of 5

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -9,14 +9,34 @@ class Fixnum
 	end
 
 	def array_numerals_to_roman(array)
-		return 'I' if array[-1] == '1'
-		return 'II' if array[-1] == '2'
-		return 'III' if array[-1] == '3'
-		return 'IV' if array[-1] == '4'
-		return 'V' if array[-1] == '5'
-		return 'VI' if array[-1] == '6'
-		return 'VII' if array[-1] == '7'
-		return 'VIII' if array[-1] == '8'
-		return 'IX' if array[-1] == '9'
+		"#{second_digit(array[-2])}#{first_digit(array[-1])}"
 	end
+
+	def first_digit(string)
+		return 'I' if string == '1'
+		return 'II' if string == '2'
+		return 'III' if string == '3'
+		return 'IV' if string == '4'
+		return 'V' if string == '5'
+		return 'VI' if string == '6'
+		return 'VII' if string == '7'
+		return 'VIII' if string == '8'
+		return 'IX' if string == '9'
+		return '' if string == '0'
+	end
+
+	def second_digit(string)
+		return 'X' if string == '1'
+		return 'XX' if string == '2'
+		return 'XXX' if string == '3'
+		return 'XL' if string == '4'
+		return 'L' if string == '5'
+		return 'LX' if string == '6'
+		return 'LXX' if string == '7'
+		return 'LXXX' if string == '8'
+		return 'XC' if string == '9'
+		return '' if string == '0'
+	end
+
+
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -13,8 +13,37 @@ describe 'the roman numeral method' do
 		expect(8.roman_numeral).to eq 'VIII'
 		expect(9.roman_numeral).to eq 'IX'
 	end
-end
 
+	it "can count to 10" do
+		expect(10.roman_numeral).to eq 'X'
+	end
+
+	it "can count to 20" do
+		expect(11.roman_numeral).to eq 'XI'
+		expect(12.roman_numeral).to eq 'XII'
+		expect(13.roman_numeral).to eq 'XIII'
+		expect(14.roman_numeral).to eq 'XIV'
+		expect(15.roman_numeral).to eq 'XV'
+		expect(16.roman_numeral).to eq 'XVI'
+		expect(17.roman_numeral).to eq 'XVII'
+		expect(18.roman_numeral).to eq 'XVIII'
+		expect(19.roman_numeral).to eq 'XIX'
+	end
+
+	it 'returns a string ending in V for numbers ending in 5' do
+		(1..100).select {|number| number.to_s[-1] == '5'}.each do |fiver|
+			expect(fiver.roman_numeral[-1]).to eq 'V'
+		end
+	end
+
+	it 'returns a string ending in X for numbers ending in 0' do
+		(1..100).select {|number| number.to_s[-1] == 0}.each do |tenner|
+			expect(tenner.roman_numeral[-1]).to eq 'X'
+		end
+	end
+
+
+end
 
 
 
