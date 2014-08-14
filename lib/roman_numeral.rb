551365f083d3900625b49fad09eb96be472fb0bd
David Wickes <dave.wickes@gmail.com>
2014-08-14 22:19:59 +0100

    refactored code to work off the last digit when calculating numbers from 1 to 9, still passing

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,14 +1,22 @@
 class Fixnum
 
 	def roman_numeral
-		return 'I' if self == 1
-		return 'II' if self == 2
-		return 'III' if self == 3
-		return 'IV' if self == 4
-		return 'V' if self == 5
-		return 'VI' if self == 6
-		return 'VII' if self == 7
-		return 'VIII' if self == 8
-		return 'IX' if self == 9
+		array_numerals_to_roman(decimal_to_array)
+	end
+
+	def decimal_to_array
+		self.to_s.chars
+	end
+
+	def array_numerals_to_roman(array)
+		return 'I' if array[-1] == '1'
+		return 'II' if array[-1] == '2'
+		return 'III' if array[-1] == '3'
+		return 'IV' if array[-1] == '4'
+		return 'V' if array[-1] == '5'
+		return 'VI' if array[-1] == '6'
+		return 'VII' if array[-1] == '7'
+		return 'VIII' if array[-1] == '8'
+		return 'IX' if array[-1] == '9'
 	end
 end
