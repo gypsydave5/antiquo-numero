b4932187684cb2e1dc2a5843dad31e6285938ca8
David Wickes <dave.wickes@gmail.com>
2014-08-14 21:02:08 +0100

    tests written for numbers one to five, and a generic test for numbers ending in five divised

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,7 +1,14 @@
 class Fixnum
 
 	def roman_numeral
-	return 'I' if self == 1
-		'II' if self == 2
+		return 'I' if self == 1
+		return 'II' if self == 2
+		return 'III' if self == 3
+		return 'IV' if self == 4
+		return 'V' if self == 5
+		return 'XV' if self == 15
+		return 'XXV' if self == 25
+		return 'XXXV' if self == 35
 	end
+
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -2,13 +2,29 @@ require 'roman_numeral'
 
 describe 'a roman numeral' do
 
-	it "returns 'I' when it gets 1" do
+	it "returns 'I' when it is 1" do
 		expect(1.roman_numeral).to eq 'I'
 	end
 
-	it "returns 'II' when it gets 2" do
+	it "returns 'II' when it is 2" do
 		expect(2.roman_numeral).to eq 'II'
 	end
+
+	it "returns 'III' when it is 3" do
+		expect(3.roman_numeral).to eq 'III'
+	end
+
+	it "returns 'IV' when it is 4" do
+		expect(4.roman_numeral).to eq 'IV'
+	end
+
+	it "returns a string ending in 'V' when the number is divisible by 5" do
+		expect(5.roman_numeral[-1]).to eq 'V'
+		expect(15.roman_numeral[-1]).to eq 'V'
+		expect(25.roman_numeral[-1]).to eq 'V'
+		expect(35.roman_numeral[-1]).to eq 'V'
+	end
+
 end
 
 
